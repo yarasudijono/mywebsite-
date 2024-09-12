f9fbd4ea27091f3f7c67a56a33ab46d785b492f7
yarasudijono <yara.sudijono@gmail.com>
2024-09-12 16:27:40 -0400

    updated my website!

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -8,12 +8,42 @@ import ltstill from "../films/LT/LT STILL1.png"
 import stlfstoryboard from "../films/something to look for/STLF SB2.png"
 import stlfstill1 from "../films/something to look for/stlfstill1.png"
 import stlfstill2 from "../films/something to look for/stlfstill2.png"
+import vmjstill1 from "../films/vmj/vmjstill1.png"
+import vmjstill2 from "../films/vmj/vmjstill2.png"
+import vmjstill3 from "../films/vmj/vmjstill3.png"
+import lb1 from "../films/lemonboy/lb1.png"
+import lb2 from "../films/lemonboy/lb2.png"
+import lb3 from "../films/lemonboy/lb3.png"
 
 function Movies(){
     return(
         <>
         <div id = "maincontainer">
         
+        <div>
+            <p id = "vmj">virgin mary jane</p>
+            <p id = "vmjintro">short film, 2024; co-assistant director</p>
+            <p id = "vmjplot">choir kids august and elsie ditch their final christmas concert to get high and eat taco bell. but when they accidentally pick up the wrong person's order, they have to go on a mission to find the person it belongs to.</p>
+        </div>
+
+        <div class="flexbox-container7">
+            <img src={vmjstill1} alt="vmj1" id = "vmj1"></img>
+            <img src={vmjstill2} alt="vmj2" id="vmj2"></img>
+            <img src={vmjstill3} alt="vmj3" id="vmj3"></img>
+        </div>
+
+        <div>
+            <p id = "lb">lemon boy</p>
+            <p id = "lbintro">short film, 2024; storyboard artist</p>
+            <p id = "lbplot">a college student meets her supposed soulmate at a party and forgets what he looks like the morning after, ensuing a reverse cinderella search and meeting a girl who she finds an undeniable connection to along the way.</p>
+        </div>
+
+        <div class="flexbox-container8">
+            <img src={lb1} alt="lb1" id="lb1"></img>
+            <img src={lb2} alt="lb2" id="lb2"></img>
+            <img src={lb3} alt="lb3" id="lb3"></img>
+        </div>
+
         <div>
             <p id = "gt">good times</p>
             <p id = "gtintro">short film, 2023; assistant director</p>
